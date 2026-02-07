9898a8ed8da756a669c6010858369bf625de232b
nnsahu02 <nnsahu2022@gmail.com>
2026-02-07 23:31:58 +0530

    - Added database connection setup using mongoose.

---
+++ src/config/dbConnection.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import { env } from "./env.ts";
+
+export const dbConnection = () => {
+    try {
+        mongoose.connect(env.MONGO_URI as string);
+        console.log("🍂 MongoDB is connected.");
+    } catch (error) {
+        console.log("Error connecting to database:", error);
+    }
+};
