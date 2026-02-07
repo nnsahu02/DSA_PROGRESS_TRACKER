e6c97a8bc349c14d0212e5b2a23de6d414a4389f
nnsahu02 <nnsahu2022@gmail.com>
2026-02-08 00:59:23 +0530

    - Added JWT configuration to environment variables.

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,6 +2,9 @@ import { config } from "dotenv";
 config();
 
 export const env = {
-    PORT : process.env.PORT || 3000,
-    MONGO_URI : process.env.MONGO_URI
+    PORT: process.env.PORT || 3000,
+    MONGO_URI: process.env.MONGO_URI,
+    JWT_SECRET: process.env.JWT_SECRET,
+    JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
+    REFRESH_TOKEN_EXPIRES_DAYS : process.env.REFRESH_TOKEN_EXPIRES_DAYS
 };
