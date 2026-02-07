8addf2596fe978f5273f4e31a87fcb63adbb7db1
nnsahu02 <nnsahu2022@gmail.com>
2026-02-08 01:21:04 +0530

    - Added setCookie utility function for managing access and refresh tokens.

---
+++ src/utils/cookie.util.ts
@@ -0,0 +1,18 @@
+import type { Response } from "express";
+import { env } from "../config/env.ts";
+
+export const setCookie = (res: Response, accessToken: string, refreshToken: string) => {
+    res.cookie("accessToken", accessToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: (env.JWT_EXPIRES_IN as any).endsWith("m") ? Number(env.JWT_EXPIRES_IN!.slice(0, -1)) * 60 * 1000 : Number(env.JWT_EXPIRES_IN) * 1000
+    });
+
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: Number(env.REFRESH_TOKEN_EXPIRES_DAYS) * 24 * 60 * 60 * 1000
+    });
+}
