e95c2aa459798ee5b6f827988016653e76f3ca5e
nnsahu02 <nnsahu2022@gmail.com>
2026-02-08 19:42:33 +0530

    - Fixed user ID retrieval in getMyProfile and implement updateMyProfile functionality.

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -3,7 +3,7 @@ import { UserModel } from "./user.model.ts";
 
 export const getMyProfile = async (req: Request | any, res: Response) => {
     try {
-        const userId = req.user.id;
+        const userId = req.user._id;
 
         const user = await UserModel.findById(userId)
 
@@ -23,6 +23,46 @@ export const getMyProfile = async (req: Request | any, res: Response) => {
         res.status(500).json({
             success: false,
             message: "Failed to fetch profile",
+            error: error.message
+        });
+    }
+};
+
+export const updateMyProfile = async (req: Request | any, res: Response) => {
+    try {
+        const userId = req.user._id;
+        const { name } = req.body;
+
+        if (!name) {
+            return res.status(400).json({
+                success: false,
+                message: "Name is required"
+            });
+        }
+
+        const updatedUser = await UserModel.findByIdAndUpdate(
+            userId,
+            { name },
+            { new: true }
+        );
+
+        if (!updatedUser) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Profile updated successfully",
+            data: updatedUser
+        });
+    } catch (error: any) {
+        console.error("updateMyProfile error:", error);
+        res.status(500).json({
+            success: false,
+            message: "Failed to update profile",
             error: error.message
         });
     }
--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { authentication } from "../../middlewares/auth.middleware.ts";
-import { getMyProfile } from "./user.controller.ts";
+import { getMyProfile, updateMyProfile } from "./user.controller.ts";
 const userRouter = Router();
 
-userRouter.get("/me", authentication, getMyProfile)
+userRouter.get("/me", authentication, getMyProfile);
+userRouter.patch("/me", authentication, updateMyProfile);
 
 export default userRouter;
