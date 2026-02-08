526e13ec46c3316b797f07815a24d69ecf8d611f
nnsahu02 <nnsahu2022@gmail.com>
2026-02-09 02:02:47 +0530

    - Added CORS support to the Express app and updated package dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import router from './routes/routes.ts';
 import { env } from './config/env.ts';
@@ -7,6 +8,26 @@ import { dbConnection } from './config/dbConnection.ts';
 import { seedDataInDb } from './seed/seed.ts';
 
 const app = express();
+const allowedOrigins = [
+    "http://localhost:5173"
+];
+app.use(
+    cors({
+        origin: (origin, callback) => {
+            // allow server-to-server & tools like Postman
+            if (!origin) return callback(null, true);
+
+            if (allowedOrigins.includes(origin)) {
+                return callback(null, true);
+            }
+
+            return callback(new Error("Not allowed by CORS"));
+        },
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Authorization", "Content-Type"]
+    })
+);
 app.use(express.json());
 app.use(morgan("dev"))
 app.use(cookieParser());
