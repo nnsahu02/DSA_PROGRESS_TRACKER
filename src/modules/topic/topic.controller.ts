1b77ff5e436d701993012e771a9fcc2818731d72
nnsahu02 <nnsahu2022@gmail.com>
2026-02-09 02:04:00 +0530

    - Enhance getAllTopics to aggregate topics with problem and progress data for user-specific insights

--- src/modules/topic/topic.controller.ts
+++ src/modules/topic/topic.controller.ts
@@ -1,9 +1,87 @@
 import type { Request, Response } from "express";
 import { TopicModel } from "./topic.model.ts";
+import mongoose from "mongoose";
 
-export const getAllTopics = async (_req: Request, res: Response) => {
+export const getAllTopics = async (req: any, res: Response) => {
     try {
-        const topics = await TopicModel.find().sort({ order: 1 });
+        const userId = new mongoose.Types.ObjectId(req.user.id);
+
+        const topics = await TopicModel.aggregate([
+            { $sort: { order: 1 } },
+
+            {
+                $lookup: {
+                    from: "problems",
+                    localField: "_id",
+                    foreignField: "topicId",
+                    as: "problems"
+                }
+            },
+
+            {
+                $lookup: {
+                    from: "progresses",
+                    let: { problemIds: "$problems._id" },
+                    pipeline: [
+                        {
+                            $match: {
+                                $expr: {
+                                    $and: [
+                                        { $in: ["$problemId", "$$problemIds"] },
+                                        { $eq: ["$userId", userId] }
+                                    ]
+                                }
+                            }
+                        }
+                    ],
+                    as: "completedProgress"
+                }
+            },
+
+            {
+                $addFields: {
+                    totalProblems: { $size: "$problems" },
+                    completedProblems: { $size: "$completedProgress" }
+                }
+            },
+
+            {
+                $addFields: {
+                    remainingProblems: {
+                        $subtract: ["$totalProblems", "$completedProblems"]
+                    },
+                    progressPercent: {
+                        $cond: [
+                            { $eq: ["$totalProblems", 0] },
+                            0,
+                            {
+                                $round: [
+                                    {
+                                        $multiply: [
+                                            {
+                                                $divide: [
+                                                    "$completedProblems",
+                                                    "$totalProblems"
+                                                ]
+                                            },
+                                            100
+                                        ]
+                                    },
+                                    2
+                                ]
+                            }
+                        ]
+                    }
+                }
+            },
+
+            {
+                $project: {
+                    problems: 0,
+                    completedProgress: 0
+                }
+            }
+        ]);
 
         res.status(200).json({
             success: true,
@@ -17,3 +95,4 @@ export const getAllTopics = async (_req: Request, res: Response) => {
         });
     }
 };
+
