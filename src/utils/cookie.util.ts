b0670e7d5a5fec5cb52d9213b6937d1398e71924
nnsahu02 <nnsahu2022@gmail.com>
2026-02-09 23:09:25 +0530

    - Cookie handle.

--- src/utils/cookie.util.ts
+++ src/utils/cookie.util.ts
@@ -4,15 +4,15 @@ import { env } from "../config/env";
 export const setCookie = (res: Response, accessToken: string, refreshToken: string) => {
     res.cookie("accessToken", accessToken, {
         httpOnly: true,
-        secure: true,
-        sameSite: "strict",
+        secure: false,
+        sameSite: "lax",
         maxAge: (env.JWT_EXPIRES_IN as any).endsWith("m") ? Number(env.JWT_EXPIRES_IN!.slice(0, -1)) * 60 * 1000 : Number(env.JWT_EXPIRES_IN) * 1000
     });
 
     res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
-        secure: true,
-        sameSite: "strict",
+        secure: false,
+        sameSite: "lax",
         maxAge: Number(env.REFRESH_TOKEN_EXPIRES_DAYS) * 24 * 60 * 60 * 1000
     });
 };
